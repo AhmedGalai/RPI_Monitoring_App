63ab2a6bb83c85fc6d170ab29489228072f0bfa0
Ahmed <AhmedG369@gmail.com>
2022-11-24 03:25:19 +0100

    automatic commit

--- esp32/mqttcode.ino
+++ esp32/mqttcode.ino
@@ -1,82 +1,171 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-date-time-ntp-client-server-arduino/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
-
-#include <WiFi.h>
-#include "time.h"
-
-const char* ssid     = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
-
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 0;
-const int   daylightOffset_sec = 3600;
-
-void setup(){
-  Serial.begin(115200);
-
-  // Connect to Wi-Fi
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+#include <Adafruit_Sensor.h> //Library for Adafruit sensors , we are using for DHT
+#include <DHT_U.h> //DHT library which uses some func from Adafruit Sensor library
+#include <ESP8266WiFi.h> //library for using ESP8266 WiFi 
+#include <PubSubClient.h> //library for MQTT
+#include <ArduinoJson.h> //library for Parsing JSON
+ 
+//defining Pins
+#define DHTPIN 5
+#define LED D2
+ 
+//DHT parameters
+#define DHTTYPE    DHT11     // DHT 11
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+ 
+//MQTT Credentials
+const char* ssid = "ssid";//setting your ap ssid
+const char* password = "password";//setting your ap psk
+const char* mqttServer = "iot.reyax.com"; //MQTT URL
+const char* mqttUserName = "mqtt username";  // MQTT username
+const char* mqttPwd = "mqtt password";  // MQTT password
+const char* clientID = "username0001"; // client id username+0001
+const char* topic = "Tempdata"; //publish topic
+ 
+//parameters for using non-blocking delay
+unsigned long previousMillis = 0;
+const long interval = 5000;
+ 
+String msgStr = "";      // MQTT message buffer
+ 
+float temp, hum;
+ 
+ 
+//setting up wifi and mqtt client
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup_wifi() {
+  delay(10);
+ 
   WiFi.begin(ssid, password);
+ 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+ 
   Serial.println("");
-  Serial.println("WiFi connected.");
-  
-  // Init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
-
-  //disconnect WiFi as it's no longer needed
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
 }
-
-void loop(){
-  delay(1000);
-  printLocalTime();
-}
-
-void printLocalTime(){
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
+ 
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect(clientID, mqttUserName, mqttPwd)) {
+      Serial.println("MQTT connected");
+ 
+      client.subscribe("lights");
+      Serial.println("Topic Subscribed");
+    }
+    else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);  // wait 5sec and retry
+    }
+ 
   }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  Serial.print("Day of week: ");
-  Serial.println(&timeinfo, "%A");
-  Serial.print("Month: ");
-  Serial.println(&timeinfo, "%B");
-  Serial.print("Day of Month: ");
-  Serial.println(&timeinfo, "%d");
-  Serial.print("Year: ");
-  Serial.println(&timeinfo, "%Y");
-  Serial.print("Hour: ");
-  Serial.println(&timeinfo, "%H");
-  Serial.print("Hour (12 hour format): ");
-  Serial.println(&timeinfo, "%I");
-  Serial.print("Minute: ");
-  Serial.println(&timeinfo, "%M");
-  Serial.print("Second: ");
-  Serial.println(&timeinfo, "%S");
-
-  Serial.println("Time variables");
-  char timeHour[3];
-  strftime(timeHour,3, "%H", &timeinfo);
-  Serial.println(timeHour);
-  char timeWeekDay[10];
-  strftime(timeWeekDay,10, "%A", &timeinfo);
-  Serial.println(timeWeekDay);
+ 
+}
+ 
+//subscribe call back
+void callback(char*topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  String data = "";
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    data += (char)payload[i];
+  }
   Serial.println();
+  Serial.print("Message size :");
+  Serial.println(length);
+  Serial.println();
+  Serial.println("-----------------------");
+  Serial.println(data);
+ 
+if(data=="ON"){
+  Serial.println("LED");
+ digitalWrite(LED, HIGH);
+}
+else{
+  digitalWrite(LED, LOW);
+}
+}
+ 
+ 
+void setup() {
+  Serial.begin(115200);
+  // Initialize device.
+  dht.begin();
+  // get temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
+ 
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+ 
+  setup_wifi();
+ 
+  client.setServer(mqttServer, 1883); //setting MQTT server
+  client.setCallback(callback); //defining function which will be called when message is received.
+ 
+}
+ 
+void loop() {
+  if (!client.connected()) { //if client is not connected
+    reconnect(); //try to reconnect
+  }
+  client.loop();
+ 
+  unsigned long currentMillis = millis(); //read current time
+ 
+  if (currentMillis - previousMillis >= interval) { //if current time - last time > 5 sec
+    previousMillis = currentMillis;
+ 
+    //read temp and humidity
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+ 
+ 
+    if (isnan(event.temperature)) {
+      Serial.println(F("Error reading temperature!"));
+    }
+    else {
+      Serial.print(F("Temperature: "));
+      temp = event.temperature;
+      Serial.print(temp);
+      Serial.println(F("xC"));
+    }
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      Serial.println(F("Error reading humidity!"));
+    }
+    else {
+      Serial.print(F("Humidity: "));
+      hum = event.relative_humidity;
+      Serial.print(hum);
+      Serial.println(F("%"));
+    }
+ 
+ 
+    msgStr = String(temp) +","+String(hum);
+    byte arrSize = msgStr.length() + 1;
+    char msg[arrSize];
+ 
+    Serial.print("PUBLISH DATA:");
+    Serial.println(msgStr);
+    msgStr.toCharArray(msg, arrSize);
+    client.publish(topic, msg);
+    msgStr = "";
+    delay(50);
+ 
+  }
+ 
 }
